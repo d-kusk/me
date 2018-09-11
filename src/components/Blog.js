8da4d49cca3d46602ea21512a0af1de40def3e82
Daisuke KONISHI <skydai1151@gmail.com>
2018-09-12 01:12:07 +0900

    add blog list style

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,13 +4,11 @@ import { BlogAction } from '../action/BlogAction'
 import { Button } from './style/Button'
 import { SubTitle } from './style/Title'
 import { Text } from './style/Text'
-import Image from './Image'
 import {
   SeparateSection,
   SeparateHead,
   SeparateContent
 } from '../layouts/SeparateSection'
-import blogImage from '../static/image/img-blog.png'
 
 const BlogArea = styled.div``
 
@@ -23,28 +21,46 @@ const BlogList = styled.ul`
 
 const BlogItem = styled.li`
   width: 100%;
+  min-height: 140px;
+  border: 1px solid #f1f1f1;
+  transition: 0.2s ease-out 0s;
+
+  &:hover {
+    transform: translate3d(0, -3px, 0);
+    box-shadow: 0px 8px 10px #d5d5d5;
+  }
 
   @media (min-width: 768px) {
-    width: 33%;
+    width: 32%;
+    margin-left: 0;
+    margin-right: 0;
   }
-  padding: 1% 2%;
 
   & + & {
-    border-top: 2px solid #d5d5d5;
-    @media (min-width: 768px) {
-      border-top: none;
-      border-left: 2px solid #d5d5d5;
+    margin-left: 1%;
+
+    @media (max-width: 768px) {
+      margin-left: 0;
     }
   }
+  > div {
+    display: table;
+    width: 100%;
+    height: 100%;
+  }
   a {
+    display: table-cell;
+    vertical-align: middle;
     text-decoration: none;
+    padding: 0 5%;
   }
 
   .title {
+    display: block;
     line-height: 1.6em;
     margin-bottom: 0;
     font-size: 1rem;
-    color: #29abe2;
+    color: #7b7b7b;
   }
 
   .pubtime {
@@ -57,9 +73,12 @@ const Blog = props => {
   const blog = props.blog
   return (
     <BlogItem>
-      <a href={blog.link} target="_blank">
-        <p className="title">{blog.title}</p>
-      </a>
+      <div>
+        <a href={blog.link} target="_blank">
+          <span className="title">{blog.title}</span>
+          <time className="pubtime">{blog.pubDate}</time>
+        </a>
+      </div>
     </BlogItem>
   )
 }
@@ -73,17 +92,17 @@ class BlogSection extends Component {
         // {
         //   title: 'Test Title',
         //   link: 'Link',
-        //   time: 'Sun, 12 Aug 2018 00:28:57 +0900'
+        //   pubDate: 'Sun, 12 Aug 2018 00:28:57 +0900'
         // },
         // {
         //   title: 'Test Title',
         //   link: 'Link',
-        //   time: 'Sun, 12 Aug 2018 00:28:57 +0900'
+        //   pubDate: 'Sun, 12 Aug 2018 00:28:57 +0900'
         // },
         // {
         //   title: 'Test Title',
         //   link: 'Link',
-        //   time: 'Sun, 12 Aug 2018 00:28:57 +0900'
+        //   pubDate: 'Sun, 12 Aug 2018 00:28:57 +0900'
         // }
       ]
     }
@@ -103,12 +122,21 @@ class BlogSection extends Component {
         const item = {}
         item.title = article.title
         item.link = article.link
+        item.pubDate = this.formatDate(article.pubDate)
         latestArticle.push(item)
       })
       this.setState({
         articles: latestArticle
       })
     }
+  }
+
+  formatDate(date) {
+    const Day = new Date(date)
+    const year = Day.getFullYear()
+    const month = Day.getMonth()
+    const day = Day.getDate()
+    return `${year}.${month}.${day}`
   }
 
   render() {
@@ -127,18 +155,17 @@ class BlogSection extends Component {
             </Button>
           </SeparateHead>
           <SeparateContent>
-            <Image src={blogImage} alt="PengNoteのスクリーンショット" />
+            <div className="body">
+              {this.state.articles.length > 0 ? (
+                <BlogList>
+                  {this.state.articles.map((blog, index) => {
+                    return <Blog key={index} blog={blog} />
+                  })}
+                </BlogList>
+              ) : null}
+            </div>
           </SeparateContent>
         </SeparateSection>
-        <div className="body">
-          {this.state.articles.length > 0 ? (
-            <BlogList>
-              {this.state.articles.map((blog, index) => {
-                return <Blog key={index} blog={blog} />
-              })}
-            </BlogList>
-          ) : null}
-        </div>
       </BlogArea>
     )
   }
